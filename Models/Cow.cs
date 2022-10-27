e36013b475d818c991d73f41b1c7e00fc1342b97
Andrei-Alin Sarbu <andr56f9@edu.zealand.dk>
2022-10-27 10:12:43 +0200

    cow class added

---
+++ Models/Cow.cs
@@ -0,0 +1,26 @@
+﻿namespace FarmforCD.Models
+{
+    public class Cow
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name cannot be null");
+            }
+            if(Name.Length <3)
+            {
+                throw new ArgumentOutOfRangeException("Name cannot be less than 3 characters");
+            }
+        }    
+
+        override
+        public string ToString()
+        {
+            return $"Cow: {Name} weighs {Weight} pounds.";
+        }
+    }
+}
